d36eab3c7cacba383793a931d277540611966d0f
Scosh2021 <88459826+Scosh2021@users.noreply.github.com>
2021-12-21 19:42:05 +0530

    Added new file message

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import Homefinal from './Homefinal';
 import Media from 'react-media'
+import Message from './message';
 
 function App() {
   return (
     <div className="App">
       <Media query="(min-width: 1000px)">
         {(matches) =>{
-          return matches ? <Homefinal/> : "Hello World";
+          return matches ? <Homefinal/> : <Message/>;
         }}
       </Media>
     </div>
---
+++ src/message.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default function Message() {
+    return (
+        <div className='wrapper'>
+            <h2 className='text-light message'>The Mobile/Tablet Version of this site is under maintanence. Please Try to open on a Bigger Screen.</h2>
+        </div>
+    )
+}
